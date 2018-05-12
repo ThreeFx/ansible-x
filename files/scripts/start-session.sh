cbe1954f4909bd0dc312241bb6aa4a9928ced942
ThreeFx <ben@sjfiedler.de>
2018-05-12 18:30:43 +0200

    Cosmetic fix

--- files/scripts/start-session.sh
+++ files/scripts/start-session.sh
@@ -2,7 +2,7 @@
 
 sleep 1
 
-trayer --align right --edge bottom --widthtype request --height 20 \
+trayer --align right --edge bottom --widthtype request --height 19 \
     --transparent true --tint 0x00000000 &
 
 nm-applet &
